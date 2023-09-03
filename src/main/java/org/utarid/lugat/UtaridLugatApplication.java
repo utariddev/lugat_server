9d9b79b80e77c4c0b5438802fe4c8ca761e9d1c1
utarid <utarid@protonmail.com>
2023-09-03 21:38:39 +0200

    initial commit

---
+++ src/main/java/org/utarid/lugat/UtaridLugatApplication.java
@@ -0,0 +1,13 @@
+package org.utarid.lugat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtaridLugatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtaridLugatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/utarid/lugat/UtaridLugatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.utarid.lugat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtaridLugatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
